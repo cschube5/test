37ffa398731997c338e66bf25712bd7b0f837eeb
ChristopherSchubert <cschube5@jhu.edu>
2017-02-25 22:04:37 -0500

    chat

---
+++ src/client.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.io.*;
+import java.net.*;
+
+/**
+ * Created by ChristopherSchubert on 2/25/17.
+ */
+public class client {
+    public static void main(String[] args) {
+        try{
+            Socket s = new Socket("10.0.0.10",1201);
+            DataInputStream din = new DataInputStream(s.getInputStream());
+            DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            String msgin="",msgout="";
+            while (!msgin.equals("end")){
+                msgout = br.readLine();
+                dout.writeUTF(msgout);
+                msgin = din.readUTF();
+                System.out.println(msgin);
+            }
+        }catch (Exception e){
+
+        }
+    }
+}
---
+++ src/server.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.io.*;
+import java.net.*;
+
+/**
+ * Created by ChristopherSchubert on 2/25/17.
+ */
+public class server {
+
+    public static void main(String[] args) {
+        try{
+
+            ServerSocket ss = new ServerSocket(1201);
+            Socket s = ss.accept();
+
+            DataInputStream din = new DataInputStream(s.getInputStream());
+            DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+            String msgin=" ",msgout="";
+
+            while(!msgin.equals("end")){
+                msgin = din.readUTF();
+                System.out.println(msgin);
+                msgout = br.readLine();
+                dout.writeUTF(msgout);
+                dout.flush();
+            }
+            s.close();
+        }catch (Exception e){
+            //handle exceptions
+        }
+    }
+}
